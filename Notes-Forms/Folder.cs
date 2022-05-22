08b82b072d252b20ca16aa95ef0bf17f030f6f14
DavidLG092 <davi91002@hotmail.com>
2022-05-22 13:42:03 -0300

    Classes Note e Folder
    Classes Note e Folder foram importadas do projeto original.

---
+++ Notes-Forms/Folder.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Notes
+{
+    internal class Folder
+    {
+        // Variáveis de diretório
+        private static string mainPath;
+        private static string currentPath;
+        private static string date;
+
+        // Variáveis de arquivo
+        private Note n;
+        private static string fileName;
+
+        public Folder()
+        {
+            mainPath = System.Environment.CurrentDirectory + @"\Notes\";
+            currentPath = mainPath;
+            date = System.DateTime.Today.ToString("d").Replace("/", "_");
+        }
+
+        // Métodos de diretório
+
+        public void CreateDirectory(string nm = null)
+        {
+            if (nm == null || nm == "")
+            {
+                currentPath = mainPath + date;
+            }
+            else
+            {
+                currentPath = mainPath + nm;
+            }
+
+            if (!Directory.Exists(currentPath))
+            {
+                Directory.CreateDirectory(currentPath);
+                Console.WriteLine("Sucesso! Diretório {0} foi criado!", currentPath);
+            }
+            else
+            {
+                Console.WriteLine("Não foi possível criar o diretório desejado, diretório {0} já existe.", currentPath);
+            }
+        }
+
+        public void Deleteirectory(string nm = null)
+        {
+            if (nm == null || nm == "")
+            {
+                Directory.Delete(currentPath, true);
+                currentPath = mainPath;
+                Console.WriteLine("Sucesso! Diretório {0} foi deletado.", currentPath);
+            }
+            else
+            {
+                if (Directory.Exists(mainPath + nm))
+                {
+                    Directory.Delete(mainPath + nm);
+                    Console.WriteLine("Sucesso! Diretório {0} foi deletado.", mainPath + nm);
+                }
+                else
+                {
+                    Console.WriteLine("Diretório {0} não existe, não foi possível deletá-lo.", mainPath + nm);
+                }
+            }
+        }
+
+        public void MoveToDirectory(string nm)
+        {
+            currentPath = mainPath + nm;
+
+            if (!Directory.Exists(currentPath))
+            {
+                Console.WriteLine("O diretório informado não existe. Garanta que o nome tenha sido digitado corretamente.");
+            }
+            else
+            {
+                ListDirectory(currentPath);
+            }
+        }
+
+        private void ListDirectory(string nm)
+        {
+            DirectoryInfo dir = new DirectoryInfo(nm);
+            int i = 1;
+
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                Console.WriteLine("{0} - {1}", i, file.Name.Remove(file.Name.ToString().IndexOf("."), 4));
+                i++;
+            }
+        }
+
+        // Métodos de arquivos
+
+        public void CreateFile(string nm)
+        {
+            n = new Note();
+
+            if (nm == null || nm == "")
+            {
+                fileName = currentPath + date;
+            }
+            else
+            {
+                fileName = currentPath + nm;
+            }
+
+            n.Create(fileName);
+        }
+
+        public void DeleteFile(string nm)
+        {
+            n = new Note();
+
+            if (nm == null || nm == "")
+            {
+                fileName = currentPath + date;
+            }
+            else
+            {
+                fileName = currentPath + nm;
+            }
+
+            n.Delete(fileName);
+        }
+
+        public void ModifyFile(string nm)
+        {
+            n = new Note();
+
+            if (nm == null || nm == "")
+            {
+                fileName = currentPath + date;
+            }
+            else
+            {
+                fileName = currentPath + nm;
+            }
+
+            n.Modify(fileName);
+        }
+
+    }
+}
---
+++ Notes-Forms/Note.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Notes
+{
+    internal class Note
+    {
+        public void Create(string nm)
+        {
+            if (!File.Exists(nm))
+            {
+                File.Create(nm);
+                Console.WriteLine("Sucesso! O arquivo {0} foi criado!", nm);
+            }
+            else
+            {
+                Console.WriteLine("Um arquivo com esse nome já existe no diretório indicado!");
+            }
+        }
+
+        public void Delete(string nm)
+        {
+            if (File.Exists(nm))
+            {
+                Console.WriteLine("Você deseja apagar o arquivo {0}.", nm);
+                Console.Write("Tem certeza? (Y/N)\n> ");
+                var conf = Console.ReadLine();
+
+                if ((conf.ToLower().Equals("y")))
+                {
+                    File.Delete(nm);
+                    Console.WriteLine("Sucesso! O arquivo {1} foi removido!", nm);
+                }
+                else
+                {
+                    Console.WriteLine("Nenhum arquivo foi apagado!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Não existe nenhum arquivo {0} localizado neste diretório", nm);
+            }
+        }
+
+        public void Modify(string nm)
+        {
+            string fileText;
+            TextBox tb = new TextBox();
+
+            if (File.Exists(nm))
+            {
+                fileText = File.ReadAllText(nm);
+                tb.Text = fileText;
+                tb.Show();
+                Console.ReadLine();
+
+                /*
+                while (!(Console.ReadKey().Key == ConsoleKey.Enter))
+                {
+                    switch (Console.ReadKey().Key)
+                    {
+                        case ConsoleKey.UpArrow:
+                            try
+                            {
+                                Console.CursorTop -= 1;
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.CursorTop = Console.CursorTop;
+                            }
+                            break;
+                        case ConsoleKey.RightArrow:
+                            try
+                            {
+                                Console.CursorLeft -= 1;
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.CursorLeft = Console.CursorLeft;
+                            }
+                            break;
+                        case ConsoleKey.DownArrow:
+                            try
+                            {
+                                Console.CursorTop += 1;
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.CursorTop = Console.CursorTop;
+                            }
+                            break;
+                        case ConsoleKey.LeftArrow:
+                            try
+                            {
+                                Console.CursorLeft += 1;
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.CursorLeft = Console.CursorLeft;
+                            }
+                                break;
+                        case ConsoleKey.Backspace:
+                            break;
+                    }
+                }*/
+            }
+            else
+            {
+                Console.WriteLine("Não existe nenhum arquivo {0} localizado neste diretório", nm);
+            }
+        }
+    }
+}
